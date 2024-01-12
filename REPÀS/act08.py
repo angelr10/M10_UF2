a53d60562de7cfb7ca706d849e5ea2a6d63b20e7
angelr10 <andreh10155@gmail.com>
2024-01-12 14:27:03 +0100

    subida de contenido dia 2

--- REPÀS/act08.py
+++ REPÀS/act08.py
@@ -1,6 +1,10 @@
-a = insert("Agrega de 1 a 3 palabras a continuacion: ")
-b = a.length
+a = input("Agrega de 1 a 3 palabras a continuacion: ")
+b = a.split()
+while len(b)>3 or len(b)<1:
+	print("Pls introduce un numero del 1 al 3")
+	a = input("Agrega de 1 a 3 palabras a continuacion: ")
+	b = a.split()
 c = 0
-while c < b-1:
-	print("{",a[c],"}")
+while c < len(b):
+	print("Has introducido : *"+str(b[c])+"* tiene un total de "+str(len(b[c]))+" caracteres, y comienza con el caracter *"+str(b[c][0:1])+"*, y finaliza con el caracter *"+str(b[c][-1])+"*")
 	c=c+1
