c5e1b2a3e6087b07d637e02e5598f8f22a166318
angelr10 <andreh10155@gmail.com>
2024-02-04 23:34:43 +0100

    Practica 2 Angel completado el create.py

---
+++ PRACTICA2/create.py
@@ -0,0 +1,9 @@
+# Este archivo contiene las ordenes para 
+# crear un insert de informacion dentro de la base 
+# de datos.
+sql_insert = ''' INSERT INTO public.users(coche_prop_nombre, coche_prop_edad, coche_matricula,coche_modelo,coche_color)
+                VALUES('1',22,'88888888','subaru','azul')
+'''
+
+connection.execute(sql_insert)
+conn.commit()
